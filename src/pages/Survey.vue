40a310023eed05d7e2c9a90eb0a837e5763fde2a
BladeHiker <duanjihang@live.com>
2021-05-10 10:48:31 +0800

    Update Survey.vue

--- src/pages/Survey.vue
+++ src/pages/Survey.vue
@@ -1,113 +1,110 @@
 <template>
-  <q-page>
-    <q-scroll-area :thumb-style="thumbStyle"
-                   :bar-style="barStyle" class="main-con">
-      <div class="column">
-        <div class="paper-header">
-          <div class="text-h4 text-center paper-title">问卷标题{{ $route.params.token }}</div>
-          <div class="text-center paper-time">2020/1/1~2021/12/31</div>
-          <br>
-          <div class="text-center">问卷说明文字,问卷说明文字,问卷说明文字,问卷说明文字,问卷说明文字,问卷说明文字</div>
+  <q-page class="main-con">
+    <div class="column">
+      <div class="paper-header">
+        <div class="text-h4 text-center paper-title">问卷标题{{ $route.params.token }}</div>
+        <div class="text-center paper-time">2020/1/1~2021/12/31</div>
+        <br>
+        <div class="text-center">问卷说明文字,问卷说明文字,问卷说明文字,问卷说明文字,问卷说明文字,问卷说明文字</div>
+      </div>
+      <div v-for="(problem,i) in problemSet" :key="i" class="ques-section">
+        <div v-if="problem.type===0">
+          <div class="text-h6 ques-title">
+            <b>{{ problem.index }}/ </b>
+            <span>{{ problem.title }}</span>
+            <span v-if="problem.need"> *</span>
+            <q-input label="请输入" outlined v-model="answer[i]"></q-input>
+          </div>
         </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>01/ </b><span>问题xxx</span></div>
-          <q-input label="请输入" outlined></q-input>
-        </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>02/ </b><span>问题xxx</span></div>
-          <q-input label="请输入" outlined></q-input>
-        </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>03/ </b><span>问题xxx</span></div>
-          <q-input label="请输入" outlined></q-input>
-        </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>04/ </b><span>问题xxx</span></div>
-          <q-input label="请输入" outlined></q-input>
-        </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>05/ </b><span>问题xxx</span></div>
-          <q-input label="请输入" outlined></q-input>
-        </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>06/ </b><span>问题xxx</span></div>
-          <q-input label="请输入" outlined></q-input>
-        </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>07/ </b><span>单选题xxx</span></div>
+        <div v-else-if="problem.type===1">
+          <div class="text-h6 ques-title">
+            <b>{{ problem.index }}/ </b>
+            <span>{{ problem.title }}</span>
+            <span v-if="problem.need"> *</span>
+          </div>
           <q-option-group
-            v-model="group"
-            :options="options"
+            v-model="answer[i]"
+            :options="problem.options"
             color="primary"
             type="radio"
           />
         </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>08/ </b><span>问题xxx</span></div>
-          <q-input label="请输入" outlined></q-input>
-        </div>
-        <div class="ques-section">
-          <div class="text-h6 ques-title"><b>09/ </b><span>问题xxx</span></div>
-          <q-input label="请输入" outlined></q-input>
-        </div>
-        <q-btn
-          label="提交"
-          color="primary"
-          class="flex-center submit-btn"
-        />
       </div>
-      <br>
-      <q-separator/>
-      <div class="paper-footer text-center">
 
-        Copyright © 2021 LoremIpsum Team
-        <br>
-        系统由LoremSurvey提供
-      </div>
-    </q-scroll-area>
+      <q-btn
+        label="提交"
+        color="primary"
+        @click="submit"
+        class="flex-center submit-btn"
+      />
+    </div>
+    <br>
+    <q-separator/>
+    <div class="paper-footer text-center">
+
+      Copyright © 2021 LoremIpsum Team
+      <br>
+      系统由LoremSurvey提供
+    </div>
 
   </q-page>
 </template>
 
 <script>
 
+import {Notify} from "quasar";
+
 export default {
   name: "survey",
   data() {
     return {
-      group: '',
-      options: [
+      problemSet: [
         {
-          label: 'Option 1',
-          value: 'op1'
+          type: 0,
+          index: 1,
+          need: true,
+          title: "填空题示例",
         },
         {
-          label: 'Option 2',
-          value: 'op2'
+          type: 0,
+          index: 2,
+          need: false,
+          title: "填空题示例",
         },
         {
-          label: 'Option 3',
-          value: 'op3'
-        }
-      ],
-      thumbStyle: {
-        right: '0px',
-        backgroundColor: '#027be3',
-        width: '3px',
-        opacity: 0.75
-      },
-      barStyle: {
-        right: '0px',
-        backgroundColor: '#027be3',
-        width: '3px',
-        opacity: 0.2
-      }
+          type: 1,
+          index: 3,
+          need: true,
+          title: "选择题示例",
+          options: [
+            {
+              label: 'Option 1',
+              value: 'op1'
+            },
+            {
+              label: 'Option 2',
+              value: 'op2'
+            },
+            {
+              label: 'Option 3',
+              value: 'op3'
+            }
+          ],
+
+        }],
+      answer: [],
+      group:
+        '',
     }
   },
   created() {
     //鉴权
   },
-  methods: {}
+  methods: {
+    submit() {
+      Notify.create(this.answer.toString())
+    }
+  }
 }
 </script>
 
@@ -127,10 +124,9 @@ export default {
 
 .main-con {
   width: min(100vw, 800px);
-  height: 100vh;
   padding: 30px;
   background-color: #ffffff;
-  overflow: hidden;
+  overflow-x: hidden;
 }
 
 .ques-title {
